c9e2c9650d16f2c5441f6ddf32a53b493a757d15
FJHelfferich <F.J.Helfferich@student.tudelft.nl>
2020-09-22 12:57:25 +0200

    Add Python to labjack translation script

---
+++ LinMove1.py
@@ -0,0 +1,12 @@
+from labjack import ljm
+
+# Open first USB connected found LabJack
+handle = ljm.openS("ANY", "USB", "ANY")
+
+# Call eReadName to read the serial number from the LabJack.
+name = "SERIAL_NUMBER"
+result = ljm.eReadName(handle, name)
+
+print("\neReadName result: ")
+print("    %s = %s" % (name, result))
+
