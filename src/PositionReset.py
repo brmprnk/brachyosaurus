0b678e3f96a35200ac84c59a2431154d5c274146
FJHelfferich <F.J.Helfferich@student.tudelft.nl>
2020-10-05 12:01:54 +0200

    improved position reset
    belangrijk om lines 25 en 26 erin te houden, zo blijft de stage op positie nul

--- src/PositionReset.py
+++ src/PositionReset.py
@@ -6,22 +6,25 @@ import sys
 handle = ljm.openS("ANY", "ANY", "ANY")
 
 f_datatype = ljm.constants.FLOAT32
-offsetV = 0                         # Volt
+offsetV = 2.5                         # Volt
 sc = 2                            # scaling factor
 
-# cycle through positive, negative and zero-value voltages on the DACs
-# first test is also INIT
-print("\nINIT I/O of LABjack T7 w/ write statements w/ value 0")
-print("    test 0: DAC0 = 0, DAC1 = 0")
-ljm.eWriteAddress(handle, 1000, f_datatype, 0)
-ljm.eWriteAddress(handle, 1002, f_datatype, 0)
+print("Reset: INIT DOUTs of T7")
+ljm.eWriteAddress(handle, 1000, f_datatype, offsetV)
+ljm.eWriteAddress(handle, 1002, f_datatype, offsetV)
 # time.sleep(2)
 
-print("\n    test 1: DAC0 = 1, DAC1 = 0")
-ljm.eWriteAddress(handle, 1000, f_datatype, 0)
-ljm.eWriteAddress(handle, 1002, f_datatype, 5)
+print("Reset: Move back stage to minimum")
+ljm.eWriteAddress(handle, 1002, f_datatype, offsetV - 1)
 # time.sleep(2)
+
 while True:
-    print(ljm.eReadAddress(handle, 0, f_datatype))
-    # if ljm.eReadAddress(handle, 0, f_datatype) > 9: #or ljm.eReadAddress(handle, 0, f_datatype) < 0.2:
-    #     sys.exit(0)
+    print("Reset: current position: ", ljm.eReadAddress(handle, 0, f_datatype))
+    if round(ljm.eReadAddress(handle, 0, f_datatype), 3) <= 0.032 :
+        print("Reset: End reached")
+        ljm.eWriteAddress(handle, 1000, f_datatype, offsetV)
+        ljm.eWriteAddress(handle, 1002, f_datatype, 0)
+        print("Reset: final position: ", ljm.eReadAddress(handle, 0, f_datatype))
+
+        ljm.close(handle)
+        sys.exit(0)
