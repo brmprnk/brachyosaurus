db32b000a11339b1085391cbd931a2e0757b20db
FJHelfferich <F.J.Helfferich@student.tudelft.nl>
2020-10-26 18:06:16 +0100

    Added reset function for linear motors Crouzet

--- src/reset_arduino.py
+++ src/reset_arduino.py
@@ -1,24 +1,27 @@
+# Reset the motor to zero position
 import pyfirmata
-from time import sleep
+import time
 
-# Define custom function to perform Blink action
-def blinkLED(pin, message):
-    print(message)
-    board.digital[pin].write(1)
-    sleep(1)
-    board.digital[pin].write(0)
-    sleep(1)
+stepsperrev = 24
+stepstotal = round(10/0.7*stepsperrev*1.2)
+dir = 0
 
-port = 'tty.usbserial-141220'
+port = 'COM5'
 board = pyfirmata.Arduino(port)
+
+time.sleep(2)
+dirpin = board.get_pin("d:3:o")
+steppin = board.get_pin("d:2:o")
 
 # Use iterator thread to avoid buffer overflow
 it = pyfirmata.util.Iterator(board)
 it.start()
 
-i = 0
-while i < 20:
-    blinkLED(13, "Calling blinkLED")
-    i = i + 1
+dirpin.write(dir)
+for x in range(stepstotal):
+    steppin.write(1)
+    time.sleep(0.005)
+    steppin.write(0)
+    time.sleep(0.005)
 
 board.exit()
