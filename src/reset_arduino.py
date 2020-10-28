22915e2bcfdaa3f33180a39ade149984982c884e
FJHelfferich <F.J.Helfferich@student.tudelft.nl>
2020-10-28 14:16:27 +0100

    Added control for all 4 motors

--- src/reset_arduino.py
+++ src/reset_arduino.py
@@ -10,18 +10,37 @@ port = 'COM5'
 board = pyfirmata.Arduino(port)
 
 time.sleep(2)
-dirpin = board.get_pin("d:3:o")
-steppin = board.get_pin("d:2:o")
+dirpin1 = board.get_pin("d:3:o")
+steppin1 = board.get_pin("d:2:o")
+dirpin2 = board.get_pin("d:5:o")
+steppin2 = board.get_pin("d:4:o")
+dirpin3 = board.get_pin("d:7:o")
+steppin3 = board.get_pin("d:6:o")
+dirpin4 = board.get_pin("d:9:o")
+steppin4 = board.get_pin("d:8:o")
+print("RESET: pins set")
 
 # Use iterator thread to avoid buffer overflow
 it = pyfirmata.util.Iterator(board)
 it.start()
 
-dirpin.write(dir)
+dirpin1.write(dir)
+dirpin2.write(dir)
+dirpin3.write(dir)
+dirpin4.write(dir)
+print("RESET: start running...")
 for x in range(stepstotal):
-    steppin.write(1)
+    steppin1.write(1)
+    steppin2.write(1)
+    steppin3.write(1)
+    steppin4.write(1)
     time.sleep(0.005)
-    steppin.write(0)
+    steppin1.write(0)
+    steppin2.write(0)
+    steppin3.write(0)
+    steppin4.write(0)
     time.sleep(0.005)
+
+print("RESET: finished")
 
 board.exit()
