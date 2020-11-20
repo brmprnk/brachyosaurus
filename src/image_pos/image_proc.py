a2ebb03c453d35927a821ac81003e0cd08232fba
FJHelfferich <F.J.Helfferich@student.tudelft.nl>
2020-11-20 17:05:42 +0100

    Added first version of line detection in images

--- src/image_pos/image_proc.py
+++ src/image_pos/image_proc.py
@@ -11,10 +11,54 @@ Authors:
 """
 
 import cv2
-image = cv2.imread(r"C:\Users\flore\Documents\Uni\brachyosaurus\src\image_pos\photos\black.jpeg", 0)
-rows,cols = image.shape
-rot_res = cv2.rotate(image, rotateCode=2)
-cv2.imshow('Greyscale', rot_res)
-cv2.resizeWindow('Greyscale', (round(0.25*rows), round(0.25*cols)))
-if cv2.waitKey(0) == 13:  # 13 is the Enter Key
+import numpy as np
+
+
+def reduce_size(in_image, scale_percent):
+    """"
+    Reduces the resolution of the image
+    output is another image of the same type
+    """
+    height, width = in_image.shape
+    new_height = int(height * scale_percent / 100)
+    new_width = int(width * scale_percent / 100)
+    dsize = (new_width, new_height)
+
+    return cv2.resize(in_image, dsize, interpolation=cv2.INTER_CUBIC)
+
+
+def line_detect(in_image, lines_on_image, lower_threshold, upper_threshold, theta_res, min_votes, minLL, maxLG, lpf='no', show='no'):
+    """"
+    Creates edge mask from in_image and does Hough line detection on the mask
+    thresholds: are for the Canny edge mask creation
+    lpf: optional setting to use a smoothing filter (low-pass filter) on the image
+    show: optional setting to show the edges mask
+    """
+    # smoothing image against dust particles
+    if lpf == 'yes':
+        kernel = np.ones((3,3),np.float32)/9
+        in_image = cv2.filter2D(in_image,-1,kernel)
+
+    edges = cv2.Canny(in_image, lower_threshold, upper_threshold)
+    if show == 'yes':
+        cv2.imshow("edge mask", edges)
+    lines = cv2.HoughLinesP(edges, 1, np.pi/theta_res, min_votes, minLineLength=minLL, maxLineGap=maxLG)
+
+    for line in lines:
+        x1,y1,x2,y2 = line[0]
+        cv2.line(lines_on_image, (x1, y1), (x2, y2), (200, 100, 200), 2)
+
+    return lines
+
+
+org_image = cv2.imread(r'C:\Users\flore\Documents\Uni\brachyosaurus\src\image_pos\photos\bending.jpg')
+gray = cv2.cvtColor(org_image, cv2.COLOR_BGR2GRAY)
+gray_small = reduce_size(gray, 25)
+small_color = cv2.cvtColor(gray_small, cv2.COLOR_GRAY2BGR)
+
+lines = line_detect(gray_small, small_color, 150, 210, 180, 50, 20, 10, 'yes', 'yes')
+
+cv2.imshow('org_image', small_color)
+if cv2.waitKey(0) == 13:
     cv2.destroyAllWindows()
+
